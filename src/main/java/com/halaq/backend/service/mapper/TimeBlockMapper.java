d621c7d33bb7f918da16fc5715b97c9c1d860e7b
Youssafouhba <youssefouhba@gmail.com>
2026-02-12 00:47:58 +0100

    updates of 08/02

--- src/main/java/com/halaq/backend/service/mapper/TimeBlockMapper.java
+++ src/main/java/com/halaq/backend/service/mapper/TimeBlockMapper.java
@@ -1,14 +1,20 @@
 package com.halaq.backend.service.mapper;
 
 import com.halaq.backend.core.mapper.BaseMapper;
+import com.halaq.backend.core.mapper.context.CycleAvoidingMappingContext;
 import com.halaq.backend.service.dto.TimeBlockDto;
 import com.halaq.backend.service.entity.TimeBlock;
+import com.halaq.backend.user.dto.BarberDto;
 import com.halaq.backend.user.entity.Barber;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TimeBlockMapper extends BaseMapper<TimeBlock, TimeBlockDto> {
+
+    @Mapping(target = "barber", ignore = true)
+    BarberDto toDto(Barber entity, @Context CycleAvoidingMappingContext context);
 
 }
